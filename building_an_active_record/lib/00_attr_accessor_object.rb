171100f1fc066c08cf1b4515bcf5b901f6d012d7
Steven Inouye <steveninouye@msn.com>
2018-11-09 11:32:54 -0800

    Complete 00 attr accessor object

--- building_an_active_record/lib/00_attr_accessor_object.rb
+++ building_an_active_record/lib/00_attr_accessor_object.rb
@@ -1,5 +1,13 @@
 class AttrAccessorObject
-  def self.my_attr_accessor(*names)
+  def self.my_attr_accessor(*names) # [:x, :y]
     # ...
+    names.each do |method_name|
+      define_method(method_name) do
+        instance_variable_get("@#{method_name}")
+      end
+      define_method("#{method_name}=") do |value|
+        instance_variable_set("@#{method_name}", value)
+      end
+    end
   end
 end
