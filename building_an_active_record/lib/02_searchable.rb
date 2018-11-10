b167445c9c4b29a6ff9c5cc6cb3ef145ffdc085f
Steven Inouye <steveninouye@msn.com>
2018-11-09 16:07:53 -0800

    Complete 02 searchable

--- building_an_active_record/lib/02_searchable.rb
+++ building_an_active_record/lib/02_searchable.rb
@@ -3,10 +3,20 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_str = self.hash_to_string(params, " AND ")
+    parse_all(DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_str}
+    SQL
+  )
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end
